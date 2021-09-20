7e8f857496d6ba6b46e12cf595aef96eecb040a4
khalid hussain <khalid.hussain1091@gmail.com>
2021-09-20 21:14:20 +0500

    Subject: Added models in index file to clear out the imports and exports

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,6 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
+module.exports.Products = require('./products.model');
+module.exports.Buyers = require('./buyer.model');
+module.exports.Sellers = require('./seller.model');
+module.exports.Order = require('./orders.modal');
